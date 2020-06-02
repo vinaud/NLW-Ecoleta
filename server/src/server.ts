7692d546e4c9cde03e36e38d08f9e82205480c0c
João Vinaud <leyenasd@gmail.com>
2020-06-02 08:27:16 -0300

    metodo post

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,15 @@ const app = express();
 
 app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
-    response.json(['Diego','Robson','Zé', 'Link'])
+    return response.json(['Diego','Robson','Zé', 'Link'])
+});
+
+app.post('/users', (request, response) => {
+    const user ={
+        name: 'diego',
+        email:'diego@rocket'
+    }
+    return response.json(user)
 });
 
 app.listen(3333);
