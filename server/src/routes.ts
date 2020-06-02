dae26fd427fcc7d881825feb39db455e99b3f888
João Vinaud <leyenasd@gmail.com>
2020-06-02 13:33:15 -0300

    melhoria na rotade items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,8 +5,15 @@ const routes = express.Router();
 
 routes.get('/items', async (request, response) => {
     const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        }
+    })
     
-    return response.json(items)
+    return response.json(serializedItems)
 });
 
 
