bdf6a9e1c8597e409bb5f3ca3c52b12856a597c5
João Vinaud <leyenasd@gmail.com>
2020-06-02 17:39:57 -0300

    ajustes no BD

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -16,7 +16,7 @@ class PointsController{
     
         const trx = await knex.transaction();
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1583258292688-d0213dc5a3a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name,
             email,
             whatsapp,
