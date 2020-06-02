dcbf0c3f9da871593e3c06454c23c1f6c6b648e8
João Vinaud <leyenasd@gmail.com>
2020-06-02 13:22:38 -0300

    rota para listagem de itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import knex from './database/connection'
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
     
-    return response.json({message: "Hello world"})
+    return response.json(items)
 });
 
 
