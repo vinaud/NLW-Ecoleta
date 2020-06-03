3e2d40ad5cf5387eaf2b0045d2f97c29d8949df0
João Vinaud <leyenasd@gmail.com>
2020-06-03 08:56:38 -0300

    Uso do header na página principal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header/>
         <h1>hello Warudo!</h1>
       
     </div>
