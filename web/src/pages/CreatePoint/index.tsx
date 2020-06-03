dc58a965e422b4a0f10a42c0a217feb932c37983
João Vinaud <leyenasd@gmail.com>
2020-06-03 13:09:32 -0300

    Base da página de cadastro

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -15,6 +15,99 @@ const CreatePoint = () => {
                     Voltar para home
                 </Link>
             </header>
+            <form>
+                <h1>Cadastro do <br/> ponto de coleta</h1>
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+                    <div className ="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input
+                          type="text"
+                          name="name"
+                          id="name"
+                        >
+                        </input>
+                    </div>
+                    <div className="field-group">
+                    <div className ="field">
+                        <label htmlFor="email">E-mail</label>
+                        <input
+                          type="email"
+                          name="email"
+                          id="email"
+                        >
+                        </input>
+                    </div>
+                    <div className ="field">
+                        <label htmlFor="whatsapp">Whatsapp</label>
+                        <input
+                          type="text"
+                          name="whatsapp"
+                          id="whatsapp"
+                        >
+                        </input>
+                    </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione um endereço no mapa</span>
+                    </legend>
+                    <div className="field-group">
+                       <div className ="field">
+                           <label htmlFor="uf">Estado (UF)</label>
+                           <select name="uf" id="uf">
+                               <option value="0">Selecione uma UF</option>
+                           </select>
+                       </div>
+                       <div className ="field">
+                           <label htmlFor="city">Cidade</label>
+                           <select name="city" id="city">
+                               <option value="0">Selecione uma cidade</option>
+                           </select>
+                       </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Ítens de coleta</h2>
+                        <span>Selecione um ou mais itens abaixo</span>
+                    </legend>
+                    <ul className="items-grid">
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
+                            <span>Óleo de cozinha</span>
+                        </li>
+                    </ul>
+                </fieldset>
+            </form>
         </div>
     )
 }
