d5819b889144a491f03a8799e655ce3528a2e350
João Vinaud <leyenasd@gmail.com>
2020-06-03 08:34:54 -0300

    limpeza de código

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <h1>hello Warudo!</h1>
-      </header>
+      
     </div>
   );
 }
