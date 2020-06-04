aaa4fc3c5bf793831fd198db547094b068cf8d66
João Vinaud <leyenasd@gmail.com>
2020-06-04 10:19:43 -0300

    aplicação das fontes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import Home from './src/pages/Home';
+import { Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, Roboto_500Medium, Ubuntu_700Bold
+  })
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
