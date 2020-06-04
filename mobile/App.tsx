f5e8af447660c28263be7b73e04cd53ad5284367
João Vinaud <leyenasd@gmail.com>
2020-06-04 12:37:40 -0300

    Definição das rotas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { AppLoading } from 'expo';
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 import { Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
 
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-      <Home/>
+      <Routes/>
     </>
   );
 }
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
-import { } from '@react-navigation/native;
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import Points from './pages/Points';
+
+const Appstack = createStackNavigator();
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Appstack.Navigator>
+                <Appstack.Screen name="Home" component={Home}/>
+                <Appstack.Screen name="Points" component={Points}/>
+                <Appstack.Screen name="Detail" component={Detail}/>
+            </Appstack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Routes;
