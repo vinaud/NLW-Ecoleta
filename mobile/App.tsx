84ef26f2fc28e604c71cec38f76e2dc420eb148b
João Vinaud <leyenasd@gmail.com>
2020-06-04 09:41:40 -0300

    Configuração da status bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import Home from './src/pages/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello World 2!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+      <Home/>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
