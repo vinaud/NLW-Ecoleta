6017ef1d5b5f5990d4ca2ab6b210a3923b511d59
João Vinaud <leyenasd@gmail.com>
2020-06-05 09:16:19 -0300

    aplicação do multer na rota da api

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,18 @@
 import express from 'express';
-import knex from './database/connection'
-
-import PointsController from './controllers/pointsController'
+import knex from './database/connection';
+import multer from 'multer';
+import multerConfig from './config/multer';
+import PointsController from './controllers/pointsController';
 import ItemsController from './controllers/itemsController';
+
 const routes = express.Router();
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
+const upload = multer(multerConfig);
 
 routes.get('/items', itemsController.index );
 
-routes.post('/points', pointsController.create);
+routes.post('/points',upload.single('image'), pointsController.create);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points/', pointsController.index);
 
