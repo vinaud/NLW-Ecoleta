44b9200fffe299639a8f7dedace9906a66f0cde9
João Vinaud <leyenasd@gmail.com>
2020-06-05 09:47:52 -0300

    rotas enviando imagem cadastrado

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -16,7 +16,7 @@ class PointsController{
     
         const trx = await knex.transaction();
         const point = {
-            image: 'https://images.unsplash.com/photo-1583258292688-d0213dc5a3a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
@@ -58,14 +58,23 @@ class PointsController{
 
         if(!point){
             return response.status(400).json({message: ' not found'})
+        } 
+
+        const serializedPoint = {
+            
+                ...point,
+                image_url: `http://192.168.0.14:3333/uploads/${point.image}`,
+            
         }
+        
+    
 
         const items = await knex('items')
         .join('point_items', 'items.id', '=', 'point_items.item_id')
         .where('point_items.point_id', id)
         .select('items.title');
 
-        return response.json({point, items});
+        return response.json({point: serializedPoint, items});
     }
 
     async index(request: Request, response: Response){
@@ -81,7 +90,14 @@ class PointsController{
         .distinct()
         .select('points.*');
 
-        return response.json(points);
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.0.14:3333/uploads/${point.image}`,
+            }
+        })
+        
+        return response.json(serializedPoints);
     }
 }
 
