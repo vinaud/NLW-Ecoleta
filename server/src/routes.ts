f48721593ea207dac893f9b0e76d61c66e1fd3ae
João Vinaud <leyenasd@gmail.com>
2020-06-05 11:48:40 -0300

    Validação de entradas na rota de cadastro, via Celebrate

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 import PointsController from './controllers/pointsController';
 import ItemsController from './controllers/itemsController';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express.Router();
 const pointsController = new PointsController();
@@ -11,10 +12,24 @@ const itemsController = new ItemsController();
 const upload = multer(multerConfig);
 
 routes.get('/items', itemsController.index );
-
-routes.post('/points',upload.single('image'), pointsController.create);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points/', pointsController.index);
-
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude:  Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }),
+     pointsController.create
+     );
 
 export default routes;
